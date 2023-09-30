9b04fe4e116027631c9fad51206fa856c23d8129
Vinay Dwivedi <11961529+vnydvd@users.noreply.github.com>
2023-09-30 19:38:15 +0000

    variables and datatypes day 1

---
+++ concepts/variables.js
@@ -0,0 +1,15 @@
+const name1 = "Universe"
+const value = 42
+
+// console.log(`The answer to every question in the ${name1} is ${value}`);
+
+let str1 = "planet"
+let str2 = new String("Earth")
+
+console.log(typeof str1);
+console.log(str1);
+console.log(typeof str2);
+console.log(str2);
+
+console.log(str1.toUpperCase());
+console.log(str2.toUpperCase());

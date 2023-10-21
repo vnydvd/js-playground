e9a6fbeb30a2d8ee78c7aba1491635ebb54e5c43
Vinay Dwivedi <11961529+vnydvd@users.noreply.github.com>
2023-10-21 19:50:56 +0000

    date time in JS

---
+++ concepts/date_variables.js
@@ -0,0 +1,12 @@
+let newDate = new Date();
+
+console.log(newDate);
+console.log(newDate.toString());
+console.log(newDate.toDateString());
+console.log(newDate.toLocaleString());
+
+let dateFromString = new Date("2023-10-22");
+console.log(dateFromString);
+console.log(dateFromString.toString());
+console.log(dateFromString.toDateString());
+console.log(dateFromString.toLocaleString());
